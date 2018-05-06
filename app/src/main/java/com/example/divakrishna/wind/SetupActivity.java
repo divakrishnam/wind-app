40607ac05d478270cc1b1ff3efba103335d673b5
Diva Krishna <divakrishna55@gmail.com>
2018-05-06 20:32:10 +0700

    Part 11

--- app/src/main/java/com/example/divakrishna/wind/SetupActivity.java
+++ app/src/main/java/com/example/divakrishna/wind/SetupActivity.java
@@ -4,11 +4,19 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
@@ -18,16 +26,37 @@ public class SetupActivity extends AppCompatActivity {
     private EditText mNameField;
     private Button mSubmitButton;
 
+    private Uri mImageUri = null;
+
     private static final int GALLERY_REQUEST = 1;
+
+    private FirebaseAuth mAuth;
+
+    private DatabaseReference mDatabaseUsers;
+
+    private StorageReference mStorageImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        mStorageImage = FirebaseStorage.getInstance().getReference().child("Profile_images");
+
+        mDatabaseUsers = FirebaseDatabase.getInstance().getReference().child("Users");
+
         mSetupImageButton = (ImageButton)findViewById(R.id.setupImageButton);
         mNameField = (EditText)findViewById(R.id.setupNameField);
         mSubmitButton = (Button)findViewById(R.id.setupSubmitButton);
+
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSetupAccount();
+            }
+        });
 
         mSetupImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,6 +67,29 @@ public class SetupActivity extends AppCompatActivity {
                 startActivityForResult(galleryIntent, GALLERY_REQUEST);
             }
         });
+    }
+
+    private void startSetupAccount() {
+
+        final String name = mNameField.getText().toString().trim();
+
+        final String user_id = mAuth.getCurrentUser().getUid();
+
+        if(!TextUtils.isEmpty(name) && mImageUri != null){
+
+            StorageReference filepath = mStorageImage.child(mImageUri.getLastPathSegment());
+
+            filepath.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    String downloadUri = taskSnapshot.getDownloadUrl().toString();
+
+                    mDatabaseUsers.child(user_id).child("name").setValue(name);
+                    mDatabaseUsers.child(user_id).child("image").setValue(downloadUri);
+                }
+            });
+        }
+
     }
 
     @Override
@@ -56,9 +108,9 @@ public class SetupActivity extends AppCompatActivity {
         if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
             CropImage.ActivityResult result = CropImage.getActivityResult(data);
             if (resultCode == RESULT_OK) {
-                Uri resultUri = result.getUri();
+                mImageUri = result.getUri();
 
-                mSetupImageButton.setImageURI(resultUri);
+                mSetupImageButton.setImageURI(mImageUri);
             } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
                 Exception error = result.getError();
             }
